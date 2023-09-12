bb69abcdbfcdec89368ecd47c2b9f41b42bc2f1a
xaraxx <sara.galvan@udea.edu.co>
2023-09-12 08:17:34 -0500

    creating a netbeams project

---
+++ src/main/java/com/xaraxx/bancadamia/Bancadamia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.xaraxx.bancadamia;
+
+/**
+ *
+ * @author xaraxx
+ */
+public class Bancadamia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
