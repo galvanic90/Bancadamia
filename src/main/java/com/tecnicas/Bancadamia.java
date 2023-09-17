b5574652fb3d36b76d19b6295528ab3039b2203d
xaraxx <sara.galvan@udea.edu.co>
2023-09-17 15:09:09 -0500

    adding wellcome mesage

--- src/main/java/com/tecnicas/Bancadamia.java
+++ src/main/java/com/tecnicas/Bancadamia.java
@@ -15,6 +15,16 @@ import com.tecnicas.model.CorrientAccount;
 public class Bancadamia {
 
     public static void main(String[] args) {
+        static final wellcome = """
+  ____          _   _  _____          _____          __  __ _____          
+ |  _ \   /\   | \ | |/ ____|   /\   |  __ \   /\   |  \/  |_   _|   /\    
+ | |_) | /  \  |  \| | |       /  \  | |  | | /  \  | \  / | | |    /  \   
+ |  _ < / /\ \ | . ` | |      / /\ \ | |  | |/ /\ \ | |\/| | | |   / /\ \  
+ | |_) / ____ \| |\  | |____ / ____ \| |__| / ____ \| |  | |_| |_ / ____ \ 
+ |____/_/    \_\_| \_|\_____/_/    \_\_____/_/    \_\_|  |_|_____/_/    \_\                                                                                                                                                   
+""";
+
+        
         System.out.println("Hello World!");
         System.out.println(CorrientAccount.getID());
         CorrientAccount primera= new CorrientAccount();
