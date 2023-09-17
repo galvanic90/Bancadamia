61bc9b6385141cf5d80877e6f4fa7a5b511f28b7
Ricardo Contreras <ricardo.contreras568@hotmail.com>
2023-09-17 14:45:56 -0500

    Se trabajo en los metodos de la clase CorrientAccount y Validator y se hicieron pruebas en el archivo Bancadamia

--- src/main/java/com/tecnicas/Bancadamia.java
+++ src/main/java/com/tecnicas/Bancadamia.java
@@ -15,15 +15,52 @@ import com.tecnicas.model.CorrientAccount;
 public class Bancadamia {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Se comprueba que ID este funcinando");
         System.out.println(CorrientAccount.getID());
         CorrientAccount primera= new CorrientAccount();
+        System.out.println("Se comprueba que ID este funcinando despues de instanciar un objecto");
         System.out.println(CorrientAccount.getID());
-        CorrientAccount segunda= new CorrientAccount(1000000000);
-        System.out.println(CorrientAccount.getID());
+        System.out.println("Se obtienen los datos iniciales de la instancia");
         System.out.println(primera.getBalance());
-        System.out.println(segunda.getBalance());
         System.out.println(primera.getOverdraft());
-        System.out.println(segunda.getOverdraft());
+        
+        System.out.println("Se hace un deposito");
+        primera.deposit(40000);
+        System.out.println(primera.getBalance());
+        System.out.println(primera.getOverdraft());
+        
+        System.out.println("Se hace un retiro que supere el balance");
+        primera.withdrawal(60000);
+        System.out.println(primera.getBalance());
+        System.out.println(primera.getOverdraft());
+        
+        System.out.println("Se comprueba que overdraft se rellena y el restante va para balance");
+        primera.deposit(40000);
+        System.out.println(primera.getBalance());
+        System.out.println(primera.getOverdraft());
+        
+        System.out.println("Caso de hacer un retiro invalido");
+        primera.withdrawal(-40000);
+        System.out.println(primera.getBalance());
+        System.out.println(primera.getOverdraft());
+        
+        System.out.println("Caso de hacer un deposito invalido");
+        primera.deposit(-40000);
+        System.out.println(primera.getBalance());
+        System.out.println(primera.getOverdraft());
+        
+        System.out.println("Caso de hacer un retiro con saldo insuficiente");
+        primera.withdrawal(500000);
+        System.out.println(primera.getBalance());
+        System.out.println(primera.getOverdraft());
+        
+        System.out.println("Caso de hacer un retiro que supera el monto maximo por transacción");
+        primera.withdrawal(600000);
+        System.out.println(primera.getBalance());
+        System.out.println(primera.getOverdraft());
+        
+        
+                
+        
     }
 }
