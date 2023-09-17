0cf0ed6c79f1f8e69441aace983f27f168d3a966
xaraxx <sara.galvan@udea.edu.co>
2023-09-17 17:38:16 -0500

    creating register class with atributes and methods

--- src/main/java/com/tecnicas/model/Register.java
+++ src/main/java/com/tecnicas/model/Register.java
@@ -4,10 +4,74 @@
  */
 package com.tecnicas.model;
 
+import java.util.Date;
+
 /**
  *
  * @author ricar
  */
-class Register {
- 
+public class Register {
+    private static int trasanctionId = 0;
+    private Date transactionDate; 
+    private String city;
+    private String transactionType;
+    private float amount;
+    private CorrientAccount account;
+
+    public Register(Date transactionDate, String city, String transactionType, float amount, CorrientAccount account) {
+        this.transactionDate = transactionDate;
+        this.city = city;
+        this.transactionType = transactionType;
+        this.amount = amount;
+        this.account = account;
+    }
+
+    public static int getTrasanctionId() {
+        return trasanctionId;
+    }
+
+    private static void setTrasanctionId(int trasanctionId) {
+        Register.trasanctionId += 1;
+    }
+
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public CorrientAccount getAccount() {
+        return account;
+    }
+
+    public void setAccount(CorrientAccount account) {
+        this.account = account;
+    }
+    
 }
